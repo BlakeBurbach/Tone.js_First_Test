2fbd23e43697fe8d14bf67e2c8666eeea13f5bf5
Blake Burbach <cras.bburbach@gmail.com>
2018-04-26 12:51:37 -0500

    Able to generate sounds on button click

--- tone.js_first_test/src/App.js
+++ tone.js_first_test/src/App.js
@@ -5,6 +5,11 @@ import Tone from 'tone'
 
 class App extends Component {
 
+	triggerTone = (synth, tone) => {
+		synth.triggerAttackRelease(tone);
+		synth.toMaster();
+	}
+	
   render() {
 		var synth = new Tone.Synth({
 			"oscillator" : {
@@ -16,16 +21,22 @@ class App extends Component {
 				"attackCurve" : 'exponential',
 				"attack" : 0.05,
 				"decay" : 0.2,
-				"sustain" : 0.2,
+				"sustain" : 0,
 				"release" : 1.5,
 			},
 			"portamento" : 0.05
     })
     
-    
 
     return(
-      <button onClick={()=>this.triggerTone()}>Tone</button>
+			<div>
+      	<button onClick={()=>this.triggerTone(synth, "C4")}>Tone 1</button>
+				<button onClick={()=>this.triggerTone(synth, "C#4")}>Tone 2</button>
+				<button onClick={()=>this.triggerTone(synth, "D4")}>Tone 3</button>
+				<button onClick={()=>this.triggerTone(synth, "D#4")}>Tone 4</button>
+				<button onClick={()=>this.triggerTone(synth, "E4")}>Tone 5</button>
+				<input type="range" min="1" max="100" value="50"/> 
+			</div>
     )
   }
 }
